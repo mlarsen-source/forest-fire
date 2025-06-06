8b9e63149dedd31685e44c694471407d794e9e1c
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-06-05 22:06:50 -0700

    implemented tests for timeToBurn method

--- src/FireTest.java
+++ src/FireTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class FireTest {
     @Test
-    public void testTimeToBurnExample() {
+    public void testTimeToBurnExample1() {
         char[][] forest = {
             {'t','.','.','t','t','t','t','.','t'},
             {'.','.','t','t','.','.','.','.','t'},
@@ -18,6 +18,141 @@ public class FireTest {
         int expected = 8;
         int actual = Fire.timeToBurn(forest, matchR, matchC);
 
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testTimeToBurnExample2() {
+        char[][] forest = {
+            {'t','.','.','t','t','t','t','.','t'},
+            {'.','.','t','t','.','.','.','.','t'},
+            {'.','.','t','t','t','t','t','t','t'},
+            {'t','t','t','t','.','.','.','.','.'}
+        };
+
+        int matchR = 0;
+        int matchC = 0;
+
+        int expected = 0;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testTimeToBurnExample3() {
+        char[][] forest = {
+            {'t','.','.','t','t','t','t','.','t'},
+            {'.','.','t','t','.','.','.','.','t'},
+            {'.','.','t','t','t','t','t','t','t'},
+            {'t','t','t','t','.','.','.','.','.'}
+        };
+
+        int matchR = 0;
+        int matchC = 8;
+
+        int expected = 12;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testTimeToBurnExample4() {
+        char[][] forest = {
+            {'t','.','.','t','t','t','t','.','t'},
+            {'.','.','t','t','.','.','.','.','t'},
+            {'.','.','t','t','t','t','t','t','t'},
+            {'t','t','t','t','.','.','.','.','.'}
+        };
+
+        int matchR = 3;
+        int matchC = 0;
+
+        int expected = 11;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testTimeToBurnExample5() {
+        char[][] forest = {
+            {'t','.','.','t','t','t','t','.','t'},
+            {'.','.','t','t','.','.','.','.','t'},
+            {'.','.','t','t','t','t','t','t','t'},
+            {'t','t','t','t','.','.','.','.','.'}
+        };
+
+        int matchR = 3;
+        int matchC = 3;
+
+        int expected = 8;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testForestHasNoTrees() {
+        char[][] forest = {
+            {'.','.','.','.','.','.','.','.','.'},
+            {'.','.','.','.','.','.','.','.','.'},
+            {'.','.','.','.','.','.','.','.','.'},
+            {'.','.','.','.','.','.','.','.','.'}
+        };
+
+        int matchR = 0;
+        int matchC = 0;
+
+        int expected = 0;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testForestHasOnlyTrees() {
+        char[][] forest = {
+            {'t','t','t','t','t','t','t','t','t'},
+            {'t','t','t','t','t','t','t','t','t'},
+            {'t','t','t','t','t','t','t','t','t'},
+            {'t','t','t','t','t','t','t','t','t'}
+        };
+
+        int matchR = 3;
+        int matchC = 8;
+
+        int expected = 11;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testForestIsSingleTree() {
+    char[][] forest = {
+        {'t'}
+    };
+
+    int matchR = 0;
+    int matchC = 0;
+
+        int expected = 0;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testForestIsSingleEmptySpace() {
+    char[][] forest = {
+        {'.'}
+    };
+
+    int matchR = 0;
+    int matchC = 0;
+
+        int expected = 0;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
         assertEquals(expected, actual);
     }
 }
